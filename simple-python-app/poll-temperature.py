cdaeda10e57fe95f879861814124f087a79fa6c8
hgovind <hgovind@cisco.com>
2018-08-24 11:54:26 -0700

    Update the simple python app

--- simple-python-app/poll-temperature.py
+++ simple-python-app/poll-temperature.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 import time
-
+import os
+ 
+os.makedirs("/var/volatile/log")
 f = open('/var/log/poll-temp.log', 'w')
 while (1):
     s = "%s %s polling temperature ...\n" % (time.strftime("%d/%m/%Y"), time.strftime("%I:%M:%S"))
